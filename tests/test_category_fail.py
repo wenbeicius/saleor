625e0c913862502bbc141b3358107d80144d674c
wenbeicius <wenbei@gmail.com>
2019-11-12 13:08:54 -0800

    add new tests

---
+++ tests/test_category_fail.py
@@ -0,0 +1,11 @@
+def test_product_category_filter_filters_from_child_category(
+    product_type, categories_tree
+):
+    product_filter = ProductCategoryFilter(data={}, category=categories_tree)
+    attributes = product_filter._get_attributes()
+
+    product_attr = product_type.variant_attributes.get()
+    variant_attr = product_type.variant_attributes.get()
+
+    assert product_attr in attributes
+    assert variant_attr in attribute    
